95265c56040bfc899a27e00f76a357eaa6274df7
lorenzo totis <lorenzo.totis90@gmail.com>
2025-11-20 18:27:03 +0100

    modified:   solves/network/NoisyBits.py

--- solves/network/NoisyBits.py
+++ solves/network/NoisyBits.py
@@ -1,23 +1,23 @@
 from PIL import Image
 
 img = Image.open("bits.bmp").convert("L")
-pixel_bytes = img.tobytes()
+pixel = img.tobytes()
 
 bits = []
-for p in pixel_bytes:
+for p in pixel:
     # pixel nero = 0, pixel chiaro = 1
     bit = 1 if p > 128 else 0
     bits.append(bit)
 
 # creare byte
-byte_vals = []
+byte = []
 for i in range(0, len(bits), 8):
     val = 0
     for b in bits[i:i+8]:
         val = (val << 1) | b
-    byte_vals.append(val)
+    byte.append(val)
 
-data = bytes(byte_vals)
+data = bytes(byte)
 
 with open("recovered.zip", "wb") as f:
     f.write(data)
