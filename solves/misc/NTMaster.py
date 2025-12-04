346260d49399866b9fec42d8c66419b09e584372
lorenzo totis <lorenzo.totis90@gmail.com>
2025-12-04 17:17:13 +0100

    new file:   solves/misc/NTMaster.py

---
+++ solves/misc/NTMaster.py
@@ -0,0 +1,11 @@
+from pwn import *
+
+r = remote("nt-master.challs.olicyber.it", 11001)
+for i in range(10):
+    r.recvuntil(b"N = ")
+    input = r.recvline().decode().replace("\n","")
+    res = int(input, 10)
+    print(res)
+    r.sendline(f"{res-1} 1".encode())
+    
+print(r.recv(1000).decode())
