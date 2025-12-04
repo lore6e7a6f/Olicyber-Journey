02d20bd397379d6ba8da4fee544bc01dd74c99fd
lore6e7a6f <142683062+lore6e7a6f@users.noreply.github.com>
2025-12-04 21:34:32 +0100

    Print loop index instead of result
    Change output from result to loop index.

--- solves/misc/NTMaster.py
+++ solves/misc/NTMaster.py
@@ -5,7 +5,7 @@ for i in range(10):
     r.recvuntil(b"N = ")
     input = r.recvline().decode().replace("\n","")
     res = int(input, 10)
-    print(res)
+    print(i)
     r.sendline(f"{res-1} 1".encode())
     
 print(r.recv(1000).decode())
